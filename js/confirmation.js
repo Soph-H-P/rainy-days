f2331e7267557d6ad352ff9de3d17cb418b7e0c9
Soph H.P <smkendall90@gmail.com>
2021-08-18 09:05:03 +0200

    updated info on confirmation page

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,22 +1,40 @@
 const interestingItems = document.querySelector(".other-interesting-items");
 import { productArray } from "./constants/product_list.js";
 
-const randomItem = Math.floor(Math.random() * productArray.length);
+let productsURL =
+  "https://www.soph-web-dev.eu/rainy-days/wp-json/wc/store/products?per_page=15&orderby=price&order=asc";
 
-for (let i = 0; i < productArray.length; i++) {
+const fetchAllProducts = async (url) => {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+
+    return results;
+  } catch (error) {
+    productsGrid.innerHTML = `<p class="error" >Unfortunatly we seem to be having some issues getting the product list, we appologise for any inconvenience.</p>`;
+    console.log(error);
+  }
+};
+
+const productsList = await fetchAllProducts(productsURL);
+
+const randomItem = Math.floor(Math.random() * productsList.length);
+
+for (let i = 0; i < productsList.length; i++) {
   let averageReview = getRating(productArray[i]);
+  console.log(productsList[i].prices.price.slice(0, -2))
   if (i === randomItem) {
     interestingItems.innerHTML = `
     <section class="product-card">
               <div class="product-image-wrapper">
                 <img
                   class="product-card-image"
-                  src="${productArray[i].image}"
-                  alt="${productArray[i].colour} ${productArray[i].name}"
+                  src="${productsList[i].images[0].src}"
+                  alt="${productsList[i].colour} ${productsList[i].name}"
                 />
               </div>
-              <h2>${productArray[i].name}</h2>
-              <p>£${productArray[i].price}.00</p>
+              <h2>${productsList[i].name}</h2>
+              <p>£${productsList[i].prices.price.slice(0, -2)}.00</p>
     
               <div class="customer-rating">
         <p>Customer rating:<span class="stars"><span class="rating" style="width:${averageReview}em;"></span></span></p>
@@ -24,7 +42,7 @@ for (let i = 0; i < productArray.length; i++) {
         </div>
               </div>
               <div class="view-product__button-wrapper">
-                <a id="view-product" class="cta-button" href="./product_page.html?id=${productArray[i].id}">View product</a>
+                <a id="view-product" class="cta-button" href="./product_page.html?id=${productsList[i].id}">View product</a>
               </div>
             </section>
     `;
